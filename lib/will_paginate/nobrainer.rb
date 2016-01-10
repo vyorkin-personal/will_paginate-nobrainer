4d61e9142a05d057e963e636a0ff3f953dc0d067
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-01-11 01:15:24 +0300

    Initial skeleton

---
+++ lib/will_paginate/nobrainer.rb
@@ -0,0 +1,7 @@
+require "will_paginate/nobrainer/version"
+
+module WillPaginate
+  module Nobrainer
+    # Your code goes here...
+  end
+end
---
+++ spec/will_paginate/nobrainer_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe WillPaginate::Nobrainer do
+  it 'has a version number' do
+    expect(WillPaginate::Nobrainer::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
